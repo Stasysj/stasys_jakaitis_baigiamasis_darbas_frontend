3a3a89b1ecd2e87d1d1d1b2d71ce3d64a7bbd010
Stasys <srasys@st.lt>
2022-07-27 17:05:16 +0300

    fold struc route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+import { Route, Switch } from 'react-router';
+import HomePage from './pages/Home/HomePage';
+import LoginPage from './pages/Login/LoginPage';
+import RegisterPage from './pages/Register/RegisterPage';
+
 function App() {
   return (
     <div className='App'>
       <h1>Egzaminas frontas</h1>
+
+      <Switch>
+        <Route exact path={'/'}>
+          <HomePage />
+        </Route>
+
+        <Route path={'/login'}>
+          <LoginPage />
+        </Route>
+        <Route path={'/register'}>
+          <RegisterPage />
+        </Route>
+        <Route path='*'>
+          <h2>404 Not found</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
